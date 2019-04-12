c59b79fc6e95f47bd71386ebc85088be20a3b312
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-12 16:11:36 +0200

    Update form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormComponent } from './form/form.component';
 import { RootComponent } from './root/root.component';
+import { InputComponent } from './html/input/input.component';
 
 @NgModule({
   declarations: [
     FormComponent,
-    RootComponent
+    RootComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/html/input/input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+  @Input() label: string;
+  @Input() type = 'text';
+  @Input() placeholder = '';
+  @Input() id?: string;
+
+  constructor() {
+
+
+  }
+
+  ngOnInit() {
+    if (!this.id) {
+      this.id = 'id-' + Math.ceil(Math.random() * 10000000);
+    }
+  }
+
+}
