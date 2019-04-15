63a16dfbb37cc3c136accd9ba20a2b0a76d4b11f
MUSIALIK Przemyslaw <przemyslaw.musialik@soprasteria.com>
2019-04-15 09:04:41 +0200

    Register ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormComponent } from './form/form.component';
@@ -14,7 +15,8 @@ import { InputComponent } from './html/input/input.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [RootComponent]
